879a6a68613d0abb58833aa3698d3f6c13dac5db
ertanoner <161921025+ertanoner@users.noreply.github.com>
2024-03-02 11:51:18 +0300

    Add files via upload

---
+++ 02_deneme1.py
@@ -0,0 +1,10 @@
+22+23
+print("Toplama programı")
+print(11+3)
+
+# print ile konsol ekranına birşeyler yazdırılır.
+input()  # klavyeden bilgi alma
+
+print ('Erdinç','Dönmez')
+input()
+
---
+++ 03_print deneme.py
@@ -0,0 +1,8 @@
+## print komutu/fonksiyonu içinde tek, çift, üç tırnak kullanılabilir
+print ('Merhaba') 
+print ("Nasılsın?")
+print ('''Nasılsın?''')
+#print komutunda dıştaki tırnaklar içte kullanılamaz.
+# \ veya dışta üç tek tırnak ile yapılabilir.
+print('''Eren'in annesi "gel" dedi''')
+print('Eren\'in annesi "gel" dedi')
