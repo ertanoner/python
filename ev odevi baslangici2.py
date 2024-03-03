31095d5b2da38631a58854dbc204bc360abe19bf
ertanoner <161921025+ertanoner@users.noreply.github.com>
2024-03-03 12:51:35 +0300

    Add files via upload

--- ev odevi baslangici2.py
+++ ev odevi baslangici2.py
@@ -6,21 +6,45 @@
 # 200 ╚
 # 188 ╝
 import hesaplamalar.hesapmakinesi
-import hesaplamalar.input2_toplam
-print("╔════════════════════════════════════════╗")
-print("║ VEKTOREL DERS                          ║")
-print("║                                        ║")
-print("║ 1. HESAPMAKİNESİ                       ║")
-print("║ 2. YAŞ HESABI                          ║")
-print("║ 3. ÇİZİMLER                            ║")
-print("║ 4. SAĞLIK                              ║")
-print("║                                        ║")
-print("║ 5. SEÇİMİNİZ?                          ║")
-print("╚════════════════════════════════════════╝")   
-secim = input()
-if secim == "1" :
-    hesaplamalar.hesapmakinesi.hmmenu()
-elif secim == "2" :
-    hesaplamalar.input2_toplam.yhesap()
- 
-    
+import hesaplamalar.yas_hesabi
+import hesaplamalar.oyunlar
+import hesaplamalar.renk_secimi
+import hesaplamalar.ders_secimi
+import hesaplamalar.para_birimleri
+
+def anamenu():
+    print("╔════════════════════════════════════════╗")
+    print("║ VEKTOREL DERS                          ║")
+    print("║                                        ║")
+    print("║ 1. HESAPMAKİNESİ                       ║")
+    print("║ 2. YAŞ HESABI                          ║")
+    print("║ 3. OYUNLAR                             ║")
+    print("║ 4. RENKLER                             ║")
+    print("║ 5. DERS SEÇİMİ                         ║")
+    print("║ 6. PARA BİRİMLERİ                      ║")
+    print("║                                        ║")
+    print("║    SEÇİMİNİZ?                          ║")
+    print("╚════════════════════════════════════════╝")   
+    secim = input()
+    if secim == "1" :
+        hesaplamalar.hesapmakinesi.hmmenu()
+        anamenu()
+    elif secim == "2" :
+        hesaplamalar.yas_hesabi.yhesap()
+        anamenu()
+    elif secim == "3" :
+        hesaplamalar.oyunlar.oyun()
+        anamenu()
+    elif secim == "4" :
+        hesaplamalar.renk_secimi.renk()
+        anamenu()
+    elif secim == "5" :
+        hesaplamalar.ders_secimi.ders()
+        anamenu()
+    elif secim == "6" :
+        hesaplamalar.para_birimleri.para()
+        anamenu()
+anamenu()
+
+        
+        
