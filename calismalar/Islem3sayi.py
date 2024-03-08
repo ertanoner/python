9f3fff7903ec5c50338408117d464c1bcf193091
ertanoner <161921025+ertanoner@users.noreply.github.com>
2024-03-08 08:33:44 +0300

    Add files via upload
    1. hafta sonrası evde yapılan alıştırmalar

---
+++ calismalar/Islem3sayi.py
@@ -0,0 +1,7 @@
+print("1. ve 2. sayı toplanıp, 3. sayı ile carpilacak")
+a = float(input("1.degeri giriniz:"))
+b = float(input("2. değeri giriniz:"))
+c = float(input("3. değeri giriniz:"))
+hesap = ((a+b)*c)
+print("sonuc:",hesap)
+input()
