c9d3812869cd6b54493de4c75caea8eccaf6efd2
ertanoner <161921025+ertanoner@users.noreply.github.com>
2024-03-09 12:37:23 +0300

    Add files via upload

---
+++ 2. hafta/harfnotu.py
@@ -0,0 +1,13 @@
+def harfnotu():
+    print("Bu fonksiyon harf notu hesaplar")
+    DN = int(input("Ders notunu giriniz: "))
+    if DN > 100 or DN < 0 :print("Girilen not 1-100 arasında olmalıdır.")
+    else:
+        if DN> 84 :print("A aldınız, tebrikler")
+        if DN< 85 and DN> 69 :print("B aldınız")
+        if DN< 70 and DN> 54 :print("C aldınız")
+        if DN< 55 and DN> 45 :print("D aldınız, sorumlu geçtiniz")
+        if DN< 45 :print("E aldınız, kaldınız")
+    
+                               
+    
