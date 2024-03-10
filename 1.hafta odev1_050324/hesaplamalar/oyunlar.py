c604eda18900374345d711e2eb39bd542f362a0a
ertanoner <ertanoner@gmail.com>
2024-03-10 21:07:04 +0300

    eklemeler yapildi

--- 1.hafta odev1_050324/hesaplamalar/oyunlar.py
+++ 1.hafta odev1_050324/hesaplamalar/oyunlar.py
@@ -1,13 +1,20 @@
+import yilanOyunu
+
 def oyun():
 
     print("╔════════════════════════════════════════╗")
     print("║ OYUNLAR                                ║")
     print("║                                        ║")
     print("║ 1. YILAN                               ║")
-    print("║ 2. PACMAN                              ║")
+    print("║ 2. ADAM ASMACA                         ║")
     print("║ 3. RAID                                ║")
     print("║ 4. ATEŞ                                ║")
     print("║                                        ║")
     print("║SEÇİMİNİZ?                              ║")
     print("╚════════════════════════════════════════╝")
-    input()
+    secim = input()
+    if secim == "1" :
+        yilanOyunu.yilan()
+        oyun()
+    elif secim == "2" :
+        
