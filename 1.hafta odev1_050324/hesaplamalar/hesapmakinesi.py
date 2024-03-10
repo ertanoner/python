7232d9a3718818cb03d6e80a4496e7fd483aa7db
ertanoner <ertanoner@gmail.com>
2024-03-10 20:14:45 +0300

    Hesapmakinesi islemleri tanimlandi

--- 1.hafta odev1_050324/hesaplamalar/hesapmakinesi.py
+++ 1.hafta odev1_050324/hesaplamalar/hesapmakinesi.py
@@ -10,4 +10,25 @@ def hmmenu():
     print("║                                        ║")
     print("║    SEÇİMİNİZ?                          ║")
     print("╚════════════════════════════════════════╝")
+    secim = input()
+    if secim == "1" :
+            S1 = float(input("1. sayıyı giriniz:" ))
+            S2 = float(input("2. sayıyı giriniz:" ))
+            toplam = S1+S2
+            print("Toplam:",toplam)
+    if secim == "2" :
+            S1 = float(input("1. sayıyı giriniz:" ))
+            S2 = float(input("2. sayıyı giriniz:" ))
+            fark = S1-S2
+            print("Fark:",fark) 
+    if secim == "3" :
+            S1 = float(input("1. sayıyı giriniz:" ))
+            S2 = float(input("2. sayıyı giriniz:" ))
+            carpim = S1*S2
+            print("Çarpım:",carpim)
+    if secim == "4" :
+            S1 = float(input("1. sayıyı giriniz:" ))
+            S2 = float(input("2. sayıyı giriniz:" ))
+            bolum = S1/S2
+            print("Bölüm:",bolum) 
     input()
