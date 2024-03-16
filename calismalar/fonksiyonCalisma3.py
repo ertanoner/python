86684762c24be495ad816105305c929402f87373
ertanoner <ertanoner@gmail.com>
2024-03-16 10:44:10 +0300

    düzeltme

--- calismalar/fonksiyonCalisma3.py
+++ calismalar/fonksiyonCalisma3.py
@@ -1,3 +1,5 @@
+#fonksiyonların değer döndürmesi için return komutunu kullanıyoruz
+
 def topla(aa,bb):
     return f"Toplam:{aa+bb}"
 
@@ -7,8 +9,8 @@ def carp(x,yy):
 sayi1 = int(input("1.sayı nedir?:"))
 sayi2 = int(input("2.sayı nedir?:"))
 
+# topla(sayi,sayi2) # tek başına işlemi yapar, print olmadan ekranda görünmez.
 print (topla(sayi1,sayi2))
-
 print (carp(sayi1,sayi2))
 
 
