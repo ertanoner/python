0a3f8b489437e147937a018a457060fa460c1711
ertanoner <ertanoner@gmail.com>
2024-03-16 10:30:55 +0300

    fonksiyon 3 ekleme

---
+++ calismalar/fonksiyonCalisma3.py
@@ -0,0 +1,15 @@
+def topla(aa,bb):
+    return f"Toplam:{aa+bb}"
+
+def carp(x,yy):
+    return f"Çarpım:{x*yy}"
+
+sayi1 = int(input("1.sayı nedir?:"))
+sayi2 = int(input("2.sayı nedir?:"))
+
+print (topla(sayi1,sayi2))
+
+print(carp(sayi1,sayi2))
+
+
+
