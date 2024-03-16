065a2e896015f98be458d394a37394bff411af24
ertanoner <ertanoner@gmail.com>
2024-03-16 11:08:33 +0300

    forRange

---
+++ calismalar/forRange.py
@@ -0,0 +1,11 @@
+for a in "vektörel":
+    print(a,end="_")
+
+print("")
+for a in "vektörel":
+    print(a,end="")  
+
+print("")
+meyveler = ['muz','nar','elma']
+for b in meyveler:
+    print (b)
