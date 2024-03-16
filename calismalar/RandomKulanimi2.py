ec88d134672d42bad42ecbe52ee98f1e0dcfb151
ertanoner <ertanoner@gmail.com>
2024-03-16 12:24:37 +0300

    yeni

---
+++ calismalar/RandomKulanimi2.py
@@ -0,0 +1,15 @@
+import random  
+mey = ["nar" , "muz" , "dut"]
+#random.random dersek 0 ile 1 arasında rastgele bir sayı üretir
+print(random.random())
+
+#1 ile 20 arasında bir tam sayı rastgele seç
+print(random.randint(1,20))
+
+#rastgele bir meyve ismi seçer
+print(random.choice(mey))
+
+
+# yukarıdaki girdilere göre rastgele bir cümle seçimi
+print(f"Haftalık {random.randint(1,5)}kg {random.choice(mey)} alınız.")
+
