5e5c9589445121524935ed08dc9d078a33219611
ertanoner <ertanoner@gmail.com>
2024-03-16 11:11:56 +0300

    ekleme

--- calismalar/forRange.py
+++ calismalar/forRange.py
@@ -9,3 +9,12 @@ print("")
 meyveler = ['muz','nar','elma']
 for b in meyveler:
     print (b)
+# _ herhangi bir karaktermiş gibi kabul eder. Örneğin a, x veya S1 gibi
+
+for _ in range(5):
+    print(_,"Python")
+
+print ("Tek Sayılar:",{sayi for sayi in range(1,9,2)})
+
+
+
