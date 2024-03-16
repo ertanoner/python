6ebc6f89d07aff7d0f219ceb7798758c11c2a1ed
ertanoner <ertanoner@gmail.com>
2024-03-16 10:38:41 +0300

    değişiklik

---
+++ calismalar/fonksiyonCalisma4.py
@@ -0,0 +1,15 @@
+def merhabade():   #merhabade isimli fonksiyon
+  print("Merhaba")
+  print("Nasılsın")
+  print("--------")
+
+
+def cevap():   #cevap isimli fonksiyon
+  print("İyiyim")
+  print("sen Nasılsın")
+  print("--------")
+
+
+for a in range(11):
+  merhabade()
+  cevap()
