d8d6202a01679f40b2be65d760639b62e173f891
ertanoner <ertanoner@gmail.com>
2024-03-16 11:32:06 +0300

    breakContinue

---
+++ calismalar/Break.py
@@ -0,0 +1,14 @@
+for a in range(1,11):
+    print()
+    if a == 4: continue
+
+    for b in range(1,11): 
+        
+        print(a,"x",b,"=",a*b)
+
+        #alttaki print sütun boşluğu verir
+        print()
+        # alttaki print satır boşluğu verir
+    print()    
+
+    if a == 5 : break   
