6efe9e852fe1e2189c266a7c3ef31a10a63fe42a
ertanoner <ertanoner@gmail.com>
2024-03-17 12:51:19 +0300

    ekleme

---
+++ calismalar/170324PAZ/TekrarEdenSayilarinSayisi.py
@@ -0,0 +1,16 @@
+sayilar = [2,6,5,8,7,7,1,5]
+for a in sayilar:
+    print(a)
+
+
+adet = []
+
+print("Tekrar edenler")
+for a in range(len(sayilar)):
+    for b in range(a,len(sayilar)):
+        print(sayilar[a], sayilar[b])
+        if a == sayilar[a] : sayilar[b] : print(sayilar[a])
+    #print(a)
+
+    
+    # tamamlanmamış
