6027070105ebc857d8d8ce443c15d49d0dfda59b
ertanoner <ertanoner@gmail.com>
2024-03-17 11:05:35 +0300

    ekleme

---
+++ calismalar/170324PAZ/Degisken1.py
@@ -0,0 +1,20 @@
+sayi = 3
+Sayi = 4
+sayi = 5
+sayi2 = 6.7
+# 3sayi = 7 # sayı ile başlamaz
+
+okulNumarasi = 458
+okul_numarasi = 460
+adi = "ertan"
+ogrenciAktifmi = True
+print(Sayi)
+print(sayi)
+
+print (sayi2) 
+print (type(sayi2))
+print (type(sayi))
+print (adi)
+print (type(adi))
+
+print(type(ogrenciAktifmi))
