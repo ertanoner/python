74b424c5e0c321ae9da230f0c89a58bcec20bbc1
ertanoner <ertanoner@gmail.com>
2024-03-17 11:15:00 +0300

    ekleme

---
+++ calismalar/170324PAZ/Degisken2GlobalLocal.py
@@ -0,0 +1,12 @@
+sayi1 = 3
+sayi2 = 4
+sonuc1 = 0
+sonuc2 = 0
+def topla():
+    global sonuc2
+    sonuc1 = sayi1 + sayi2
+    sonuc2 = sayi1 + sayi2
+    print("Sonuç:", sonuc1)
+    print("Sonuç:", sonuc2)
+topla()
+print("Sonuç:", sonuc)  # bu kod grubu tamamlanmadı, Hocanınkinden örnek alınacak.    
