11665f78a0bb7354855e04856e2f4f726c14afdb
ertanoner <ertanoner@gmail.com>
2024-03-17 12:31:56 +0300

    ekleme

---
+++ calismalar/170324PAZ/diziBirKisimSecmek.py
@@ -0,0 +1,14 @@
+yemek1 = "Makarna"
+yemek2 = "Mantı"
+yemek3 = "Döner"
+yemekler = [yemek1,yemek2,yemek3,"iskender", "dolma"]
+#print(yemekler)
+
+print(yemekler)
+print("Aradan Alma:", yemekler[1:3])  #indis 1 ve 2 ydeki verileri alır
+print("Aradan Alma:", yemekler[:3])   #indis 3 ten evvelki verileri alır
+print("Aradan Alma:", yemekler[3:])   #indis 3 ten itibaren verileri alır
+print("Aradan Alma:", yemekler[:])    #hepsini alır
+
+
+
