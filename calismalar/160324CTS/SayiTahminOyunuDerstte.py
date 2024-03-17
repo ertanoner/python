332bfd14c332e95f93380945a098604dc909030a
ertanoner <ertanoner@gmail.com>
2024-03-17 11:28:31 +0300

    ekleme

--- calismalar/160324CTS/SayiTahminOyunuDerstte.py
+++ calismalar/160324CTS/SayiTahminOyunuDerstte.py
@@ -2,6 +2,7 @@ import random
 bas = 1
 son = 100
 hak = 5
+puan = 100
 tutulan = random.randint(bas,son)
 print(tutulan)
 print(f"Ben {bas} ile {son} arası bir sayı tuttum.")
@@ -11,10 +12,16 @@ for xx in range(hak):
     tahmin = int(input("tahminin nedir?"))
     if tahmin == tutulan:
         print("Bildin.")
+        print("Puanın:", puan)
         break
     elif tahmin > tutulan:
         print("Tahminin büyük.")
+        puan -= 100//hak
+        print("Puanın:", puan)
         
     elif tahmin < tutulan:
         print("Tahminin küçük.")
-        
+        puan -= 100//hak
+        print("Puanın:", puan)
+
+    if xx == hak-1: print("Kaybettin")    
