a3e9b5d3ebdfe96df524a0838e27542002e1d256
ertanoner <ertanoner@gmail.com>
2024-03-18 17:29:07 +0300

    ek calismalar

---
+++ calismalar/170324PAZ/while2.py
@@ -0,0 +1,61 @@
+
+# while True:
+#     print("Deneme")
+
+# a = 5
+# while a > 3:
+#     print("Deneme")
+#     a += -1
+ 
+#a = 1
+#while a<10:
+#    print("Vektörel")
+#    a += 1
+       
+# meyveler = ["elma","armut","muz","nar"]
+# eklenecek = " "
+# while eklenecek != "":
+#     eklenecek = input("Eklenecek meyveler:")
+#     meyveler.append(eklenecek)
+#     print()
+#     print(meyveler)
+#     print(meyveler[-2])
+
+# meyveler = ["elma", "muz", "kiraz"]
+# print (meyveler)
+# meyveler.append("karpuz")
+# meyveler[2]="xx"
+# print (meyveler)
+
+# takimlar = ["Bayern München", "Bayer Leverkusen", "Borussia Dortmund"]
+# print (takimlar)
+# eklenecekTakim = " "
+# while eklenecekTakim !="":
+#     eklenecekTakim = input("yeni takim ismi giriniz.")
+#     takimlar.append(eklenecekTakim)
+#     print()
+#     print(takimlar)
+#     print(takimlar[0])
+    
+# liste = ["ar", "ara","araba", "arabalar", "arabalara"]
+# # print(liste[0])
+
+# for x in range(5):
+#     print(x, liste[x]) 
+
+    
+liste = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","y","z"]
+# print(liste.index("y"))  # y harfinin indis numarasını gösterir
+# print(liste[2:9])
+# print(liste[:])  #hepsi
+# print()
+
+
+# print(liste[-3])
+# print(liste[1:9:2])
+# print(liste[-3:-11:-2]) #sondan 3. indisten sondan başa 11. indise kadar 2 şer geri gelerek yaz.
+
+    
+    
+       
+    
