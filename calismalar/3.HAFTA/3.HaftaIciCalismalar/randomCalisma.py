381c04d5bd438c808de044872797acf887c013d8
ertanoner <ertanoner@gmail.com>
2024-03-21 17:15:13 +0300

    düzenleme

---
+++ calismalar/3.HAFTA/3.HaftaIciCalismalar/randomCalisma.py
@@ -0,0 +1,5 @@
+import random
+#print(random.random())   # random.random komutu 0 ile 1 arasında rastgele bir sayı seçer.
+
+print (random.randint(1, 10))  # random.randint komutu belirtilen rakamlar arasından bir tam sayı seçer.
+ 
