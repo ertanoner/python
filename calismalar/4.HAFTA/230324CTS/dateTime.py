e77e7998ef3335473e93d7b425312f4d51f42af4
ertanoner <ertanoner@gmail.com>
2024-03-23 10:14:00 +0300

    ekleme

--- calismalar/4.HAFTA/230324CTS/dateTime.py
+++ calismalar/4.HAFTA/230324CTS/dateTime.py
@@ -23,9 +23,30 @@
 # bg = date.today()
 # print("Bugün:", bg)
 
-import datetime
-print("Yıl=", datetime.datetime.now().strftime("%Y")) 
-a = datetime.datetime.now().strftime("%Y")               #şimdiki yıla 5 ekliyoruz
-a = int(a)+5
-print(a)
+# import datetime
+# print("Yıl=", datetime.datetime.now().strftime("%Y")) 
+# a = datetime.datetime.now().strftime("%Y")               #şimdiki yıla 5 ekliyoruz
+# a = int(a)+5
+# print(a)
 
+# import random
+# import datetime
+# gun = datetime.datetime.now().strftime("%d")      
+# tarih = random.randint(1,366)
+# print(f"{tarih} gün sonraya randevu verilecek")
+# randevu = int(gun)+tarih
+# print(randevu)
+
+# import random, datetime, time   #ileri bir tarihe randevu verme
+# print("Tarih saat =", datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+# a = datetime.datetime.now().strftime("%Y")
+# b = datetime.datetime.now().strftime("%m")
+# c = datetime.datetime.now().strftime("%d")
+# yil = int(a)+random.randint(1,5)
+# ay = 12-int(b)+random.randint(1,12)
+# gun = (30-int(c))+random.randint(1,7)
+# print(yil,ay,gun)
+
+
+# import time  # 3 saniye bekletiyor.
+# print ("Bekledim.", time.sleep(3))
