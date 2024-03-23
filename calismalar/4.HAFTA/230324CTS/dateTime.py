41ef0deb530f07fc9c3a90486c1426fa78313ad0
ertanoner <ertanoner@gmail.com>
2024-03-23 09:56:20 +0300

    ekleme

--- calismalar/4.HAFTA/230324CTS/dateTime.py
+++ calismalar/4.HAFTA/230324CTS/dateTime.py
@@ -12,3 +12,20 @@
 #   from datetime import datetime
 #   print("Şimdi =", datetime.now())
 
+# import datetime
+# print("Tarih saat =", datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"))  #gün, ay, yıl, saat, dakika, saniye den istediklerimizi belirleyerek yazdırabiliriz
+
+
+# import datetime
+# print("Gün/Ay Dakika/Saniye =", datetime.datetime.now().strftime("%d/%m %M:%S")) 
+
+# from datetime import date  #bugünü yazdırır.
+# bg = date.today()
+# print("Bugün:", bg)
+
+import datetime
+print("Yıl=", datetime.datetime.now().strftime("%Y")) 
+a = datetime.datetime.now().strftime("%Y")               #şimdiki yıla 5 ekliyoruz
+a = int(a)+5
+print(a)
+
