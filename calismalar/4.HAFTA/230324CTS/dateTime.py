7cc4d8b9de0a8228ae9f5583f6b535b7919217bb
ertanoner <ertanoner@gmail.com>
2024-03-23 09:47:24 +0300

    ekleme

---
+++ calismalar/4.HAFTA/230324CTS/dateTime.py
@@ -0,0 +1,14 @@
+# import datetime
+# print("Bugün:", datetime.date.today())  # Tarihi yazar
+
+# from datetime import datetime
+# print("Şimdi =", datetime.now())  #Şimdiki tarih ve saati gösterir
+
+# import datetime
+# print("Şimdi =", datetime.datetime.now())
+
+# from datetime import datetime    #güncel saati gösteris
+# for a in range (100000):
+#   from datetime import datetime
+#   print("Şimdi =", datetime.now())
+
