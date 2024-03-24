f720f4aa286da5194582114cae204511b74de92b
ertanoner <ertanoner@gmail.com>
2024-03-24 10:29:16 +0300

    ekleme

---
+++ calismalar/4.HAFTA/240324PZ/ArtikYilBulma.py
@@ -0,0 +1,12 @@
+# #liste içinde eleman var mı bakma
+# thislist = ["elma", "muz", "kiraz"]
+# if "elma" in thislist:
+#   print("Evet, 'elma' listede var.")
+
+# modulus 2024%4  2024 ü mod 4 e göre al kalanı göster
+
+for a in range (1900,2024):  # 1900 tılından 2024 yılına kadar artık yılları bulma
+    if a%4==0 : print(a,"artık yıl")
+    else: print (a,"artık yıl değil") 
+
+    
--- calismalar/4.HAFTA/240324PZ/TheList.py
+++
@@ -1,8 +0,0 @@
-# #liste içinde eleman var mı bakma
-# thislist = ["elma", "muz", "kiraz"]
-# if "elma" in thislist:
-#   print("Evet, 'elma' listede var.")
-
-
-for i 0 to 2024
-    
