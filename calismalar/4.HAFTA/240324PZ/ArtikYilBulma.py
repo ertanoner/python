5f96132696e2a58b3724fc4366e1630764803e8e
ertanoner <ertanoner@gmail.com>
2024-03-26 18:36:28 +0300

    düzeltme

--- calismalar/4.HAFTA/240324PZ/ArtikYilBulma.py
+++ calismalar/4.HAFTA/240324PZ/ArtikYilBulma.py
@@ -8,7 +8,7 @@
 # for a in range (1900,2024):  # 1900 tılından 2024 yılına kadar artık yılları bulma
 #     if a%4==0 : print(a,"artık yıl")
 #     else: print (a,"artık yıl değil") 
-
+#
 
 a = int(input("Bugün veya geçmiş en yakın artık yılı giriniz:"))
 for xx in range (a,-1,-4):
