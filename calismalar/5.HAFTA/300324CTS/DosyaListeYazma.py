1ee3c0181b2445e9614aa1dc910468d19ce0f320
ertanoner <ertanoner@gmail.com>
2024-03-30 11:38:29 +0300

    ekleme

---
+++ calismalar/5.HAFTA/300324CTS/DosyaListeYazma.py
@@ -0,0 +1,10 @@
+
+ogrenciler = ["Mahir", "Barış", "Şeyma", "Metin","Ertan"]
+dosya = open("deneme3.txt","w")
+dosya.write(str(ogrenciler))
+dosya.close()
+
+okunan = open("deneme3.txt")
+liste = okunan.read()
+print(liste)
+okunan.close()
