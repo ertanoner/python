99e366a44a0e3e1b7e8045f7898d0a024db56e3f
ertanoner <ertanoner@gmail.com>
2024-03-30 10:09:43 +0300

    düzeltme

--- calismalar/5.HAFTA/300324CTS/Dosya3.py
+++ calismalar/5.HAFTA/300324CTS/Dosya3.py
@@ -1,4 +1,4 @@
-#deneme dosyası oluşturulmamışsa oluşturur, varsa açıp devamına yazar. 
+#deneme dosyası oluşturulmamışsa oluşturur, varsa açıp devamına yazar. fonksiyon "f" ile fonks yazdırabiliriz.
 a=5
 with open("deneme1.txt","a") as xx:
     xx.write("\nMerhaba\n")
