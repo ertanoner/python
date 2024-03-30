190f3b846ee81e780870084814572df5935075b2
ertanoner <ertanoner@gmail.com>
2024-03-30 12:09:49 +0300

    ekleme

---
+++ calismalar/5.HAFTA/300324CTS/DosyaDictionaryYazma.py
@@ -0,0 +1,25 @@
+#yazarken string e çecirmek gerekir.
+ad = no = " "  # while da hiçbirşey yoksa çalıştırma dedik, başlangıçta birşey olsun diye boşluk ekledik.
+
+dosya = open("dosyalar/rehber.txt","a") # dosyalar klasörünü python un çalışma dosyasının içinde biz oluşturacağız.
+while ad != "" or no != "" :
+    
+    ad = input("Kaydedilecek kişi adı:")
+    no = input("Kaydedilecek no:      ")
+
+    ogrenci = {
+    "Adi" : ad ,
+    "Numarasi" : no
+    }
+    dosya.write(f"{str(ogrenci)}\n")
+
+dosya.close()
+
+
+okunan = open("dosyalar/rehber.txt")
+bb=okunan.readlines()
+aa = bb
+
+for a in aa:
+    print(a)
+okunan.close()
