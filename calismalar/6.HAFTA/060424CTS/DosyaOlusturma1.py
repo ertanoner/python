e6a7de4340e4c9a8d35fb1b3e4b10e07a73f9117
ertanoner <ertanoner@gmail.com>
2024-04-06 17:45:16 +0300

    düzeltme

---
+++ calismalar/6.HAFTA/060424CTS/DosyaOlusturma1.py
@@ -0,0 +1,14 @@
+# abc = open("rehber.txt", "w")
+# abc.write("Dosyaya kaydedilen veri.\nİkinci satır!!\n")
+# abc.write("Üçüncü satır\n") 
+# abc.write("Dördüncü satır") 
+# abc.close() # oluşturulan her dosya kapatılmalıdır.
+
+# # ör-2: Dosyaya ekleme yapma
+# f = open("rehber.txt", "a")
+# f.write("\nDosyaya ekleme yapma!")
+# f.close()
+
+# # ekleme sonrası dosya içeriğine bakma
+# f = open("rehber.txt", "r")
+# print(f.read())
