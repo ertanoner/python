4f236c3b59d9fa988c3db58601cff894bd6f9368
ertanoner <ertanoner@gmail.com>
2024-04-06 09:02:00 +0300

    ekleme

--- calismalar/6.HAFTA/060424CTS/DosyaOlusturma1.py
+++ calismalar/6.HAFTA/060424CTS/DosyaOlusturma1.py
@@ -4,3 +4,11 @@
 # abc.write("Dördüncü satır") 
 # abc.close() # oluşturulan her dosya kapatılmalıdır.
 
+# # ör-2: Dosyaya ekleme yapma
+# f = open("rehber.txt", "a")
+# f.write("\nDosyaya ekleme yapma!")
+# f.close()
+
+# ekleme sonrası dosya içeriğine bakma
+f = open("rehber.txt", "r")
+print(f.read())
