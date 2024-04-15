3970926cf2a96929ccdfc682904edfdb87916507
ertanoner <ertanoner@gmail.com>
2024-04-15 08:12:21 +0300

    ekleme

--- calismalar/6.HAFTA/6.HaftaIciCalismalar/RehberProgrami.py
+++ calismalar/6.HAFTA/6.HaftaIciCalismalar/RehberProgrami.py
@@ -19,13 +19,13 @@ def Rehbermenu():
         listele(); Rehbermenu()
     if secim=="2":
         listele(); Rehbermenu()
-    # if secim=="3":
-    #     ara(); Rehbermenu()
-    # if secim=="4":
-    #     duzelt(); listele()
-    #     Rehbermenu()
-    # if secim=="5":
-    #     sil(); listele(); Rehbermenu()
+    if secim=="3":
+        ara(); Rehbermenu()
+    if secim=="4":
+        duzelt(); listele()
+        Rehbermenu()
+    if secim=="5":
+        sil(); listele(); Rehbermenu()
         
 def kisiEkle():
     dosya = open("RehberProgrami.txt","a")
@@ -35,7 +35,7 @@ def kisiEkle():
     # kayitSayisi = dosya.read()
     # kayitSayisi = ast.literal_eval(kayitSayisi)
     # dosya.close()
-   
+
     # veri = {"adi":ad,"num":nu}
     # dosya.write(f"{ad},{nu},")
     # ks = kayitSayisi+1
@@ -47,6 +47,7 @@ def kisiEkle():
     
         # dosya.write(f'\n{str({"adi":ad,"num":no, "Kayıt Tarihi":bugun})},')
         # klasor.write(f'\n{str({"adi":ad,"numara":numara})},')
+
 def listele():
     # try:
     #     with open("RehberProgrami.txt", "r") as dosya:
@@ -55,7 +56,7 @@ def listele():
 
     #     cevirilen = ast.literal_eval(okunan)
     #     print(cevirilen)
-       
+
     # except 
     #     print("Bir hata oluştu")    
         
@@ -65,5 +66,44 @@ def listele():
 
         cevirilen = ast.literal_eval(okunan)
         print(cevirilen)      
+        
+def ara():
+    with open("RehberProgrami.txt", "r") as dosya:
+        okunan = dosya.read()
+    print("╠════════════╣ KİŞİ ARAMA ╠════════════╣")
+    cevirilen = ast.literal_eval(okunan)
+    aranan = input("Aranan isim nedir?")
+    for a in cevirilen:
+        if a["adi"]==aranan: print(a)
+        
+def duzelt():
+    with open("RehberProgrami.txt", "r") as dosya:
+        okunan = dosya.read()
+    print("╠════════════╣ KİŞİ DÜZELTME ╠════════════╣")
+    cevirilen = ast.literal_eval(okunan)
+    aranan = input("Düzeltilecek isim nedir? ")
+    dosya.close()
+    with open("RehberProgrami.txt","w") as dosya:
+        for a in cevirilen:
+            if a["adi"]==aranan:
+                print(a)
+                yeniAd = input("Yeni ad nedir? ")
+                yeniNo = input("Yeni no nedir? ")
+                a["adi"]=yeniAd
+                a["num"]=yeniNo
+            dosya.write(f"{str(a)},") 
+            
+def sil():
+    with open("RehberProgrami.txt", "r") as dosya:
+        okunan = dosya.read()
+    print("╠════════════╣ KİŞİ SİLME ╠════════════╣")
+    cevirilen = ast.literal_eval(okunan)
+    aranan = input("Silinecek isim nedir? ")
+    dosya.close()
+    with open("RehberProgrami.txt","w") as dosya:
+        for a in cevirilen:
+            if a["adi"]!=aranan:
+                dosya.write(f"{str(a)},")       
+        
         
 Rehbermenu()
