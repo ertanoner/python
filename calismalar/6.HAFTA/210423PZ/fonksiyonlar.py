e648f4b0e764fe4b422c7e75e786913941b752cb
ertanoner <ertanoner@gmail.com>
2024-04-21 09:35:32 +0300

    ekleme

---
+++ calismalar/6.HAFTA/210423PZ/fonksiyonlar.py
@@ -0,0 +1,76 @@
+# # calisma
+# x = 50
+# def test(x):
+#     print(f'x : {x}')
+
+#     x = 100
+#     print(f'changed x to {x}')
+
+# test(x)
+# print(x)
+
+
+
+# x = 50
+# def test():
+#     global x
+#     print(f'x : {x}')
+
+#     x = 100
+#     print(f'changed x to {x}')
+
+# test()
+# print(x)
+
+#iç içe fonksiyonlar
+
+# name = 'global string'
+
+# def greeting():
+#     name = 'Çınar'
+
+#     def hello():
+#         print('hello '+ name)
+
+#     hello()
+
+# greeting()
+
+
+
+# name = 'global string'
+
+# def greeting():
+#     name = 'Çınar'
+
+#     def hello():
+#         name = 'Ada'
+#         print('hello '+ name)
+
+#     hello()
+
+# greeting()
+
+
+
+# name = 'global string'
+
+# def greeting():
+#     # name = 'Çınar'
+
+#     def hello():
+#         # name = 'Ada'
+#         print('hello '+ name)
+
+#     hello()
+
+# greeting()
+
+
+# def selam():
+#     print ("merhaba")
+#     def sabah():
+#         print("günaydın")
+#     # sabah()
+
+# selam()
