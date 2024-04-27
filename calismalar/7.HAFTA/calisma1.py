078a5d57e958dec82a0f89c7c1d8e10c9747f345
ertanoner <ertanoner@gmail.com>
2024-04-27 11:14:24 +0300

    ekleme

--- calismalar/7.HAFTA/calisma1.py
+++ calismalar/7.HAFTA/calisma1.py
@@ -17,11 +17,22 @@
 # app.exec()
 
 
-# örenk 3
-import sys
-# from PyQt5.QtWidgets import QApplication, QPushButton
-from PyQt5.QtWidgets import *
-app = QApplication(sys.argv)
-window = QPushButton("Tıkla")
-window.show()
+# # örenk 3
+# import sys
+# # from PyQt5.QtWidgets import QApplication, QPushButton
+# from PyQt5.QtWidgets import *
+# app = QApplication(sys.argv)
+# window = QPushButton("Tıkla")
+# window.show()
+# app.exec()
+
+## örnek 4
+# PyQt6 yı import et
+from PyQt6.QtWidgets import *
+# Uygulama oluşturma
+app = QApplication([])
+# buraya araçlar(widgets) ekleme
+label = QLabel('Merhaba!')
+label.show()
+# Uygulamayı çalıştırma
 app.exec()
