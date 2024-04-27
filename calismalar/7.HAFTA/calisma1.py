b79204bbf43976c55f9b4f33d4f0081408ed2651
ertanoner <ertanoner@gmail.com>
2024-04-27 11:11:12 +0300

    ekleme

--- calismalar/7.HAFTA/calisma1.py
+++ calismalar/7.HAFTA/calisma1.py
@@ -1,8 +1,27 @@
 # pyqt5 commentler  https://realpython.com/python-menus-toolbars/
 # 
-# örnek 1
+## örnek 1
+# from PyQt5.QtWidgets import *
+# app = QApplication([])
+# pencere = QWidget()
+# pencere.show()
+# app.exec()
+
+# # örnek 2
+# import sys
+# from PyQt5.QtWidgets import *
+# app = QApplication(sys.argv)
+# window = QMainWindow()
+# window.show()
+# # Start the event loop.
+# app.exec()
+
+
+# örenk 3
+import sys
+# from PyQt5.QtWidgets import QApplication, QPushButton
 from PyQt5.QtWidgets import *
-app = QApplication([])
-pencere = QWidget()
-pencere.show()
+app = QApplication(sys.argv)
+window = QPushButton("Tıkla")
+window.show()
 app.exec()
