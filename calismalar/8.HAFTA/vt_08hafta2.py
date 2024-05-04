f1788f0a2ad71903a5de0f24501ea4282ba545e1
ertanoner <ertanoner@gmail.com>
2024-05-04 13:08:57 +0300

    ekleme

--- calismalar/8.HAFTA/vt_08hafta2.py
+++ calismalar/8.HAFTA/vt_08hafta2.py
@@ -1,5 +1,3 @@
-#Ë listeleme class ı eklendi. QTableWidget diye yapıda listelemeyi yapmaya çalış
-
 from PyQt6.QtWidgets import *
 
 def sifreOlustur():
@@ -23,7 +21,7 @@ class anaEkran(QMainWindow):
     icerik.addWidget(self.dugme3)
 
     self.dugme1.clicked.connect(self.ekle)
-    self.dugme2.clicked.connect(self.liste)
+    self.dugme2.clicked.connect(self.listele)
 
     araclar = QWidget()
     araclar.setLayout(icerik)
@@ -34,10 +32,11 @@ class anaEkran(QMainWindow):
     self.ekleme = EkleEkrani('Kayıt Ekleme')
     self.ekleme.show()
 
-  def liste(self):
+  def listele(self):
     self.close()
-    self.ekleme = VeriListeEkrani('Kayıt Ekleme')
-    self.ekleme.show()
+    self.listeleme = VeriListeEkrani('Kayıt Ekleme')
+    self.listeleme.show()
+
 
 
 class loginPenceresi(QMainWindow):
@@ -91,22 +90,13 @@ class EkleEkrani(QMainWindow):
     self.setWindowTitle(xx)
 
     icerik = QVBoxLayout()
-    icerik.addWidget(QLabel('Ad:'))
+    icerik.addWidget(QLabel('Ad soyad:'))
     self.edit1 = QLineEdit('')
     # self.edit1.width(50)
     icerik.addWidget(self.edit1)
-    
-    icerik.addWidget(QLabel('Soyad'))
-    self.edit2 = QLineEdit()
-    # self.edit2.setEchoMode(QLineEdit.EchoMode.Password)
-    icerik.addWidget(self.edit2)
-    
     icerik.addWidget(QLabel('Numara'))
-    self.edit3 = QLineEdit()
-    icerik.addWidget(self.edit3)
-    
-    
-    
+    self.edit2 = QLineEdit()
+    icerik.addWidget(self.edit2)
     self.dugme1 = QPushButton('Kaydet')
     icerik.addWidget(self.dugme1)
 
@@ -119,22 +109,21 @@ class EkleEkrani(QMainWindow):
   def kaydet(self):
     t1 = self.edit1.text()
     t2 = self.edit2.text()
-    t3 = self.edit3.text()
     print("Edit 1 içeriği:", t1)
     print("Edit 2 içeriği:", t2)
-    print("Edit 3 içeriği:", t3)
+    
     import sqlite3
     vt = sqlite3.connect('rehber.db')
     svt = vt.cursor()
-    svt.execute("CREATE TABLE IF NOT EXISTS isimler(id INTEGER PRIMARY KEY AUTOINCREMENT,ad,soyad,numara)")
-    svt.execute(f"INSERT INTO isimler(ad,soyad,numara) VALUES ('{t1}','{t2}','{t3}')")
+    svt.execute("CREATE TABLE IF NOT EXISTS isimler(id INTEGER PRIMARY KEY AUTOINCREMENT,ad,nu)")
+    svt.execute(f"INSERT INTO isimler(ad,nu) VALUES ('{t1}','{t2}')")
     vt.commit()
     vt.close()
 
     self.close() # mevcut pencereyi kapa
     self.ae = anaEkran() # anaekran isimli pencere tanımla
     self.ae.show() # anaekran ı göster.
-    
+
 class VeriListeEkrani(QMainWindow):
   def __init__(self,xx="Başlıksız"):
     super().__init__()
@@ -144,34 +133,32 @@ class VeriListeEkrani(QMainWindow):
     vt = sqlite3.connect('rehber.db')
     svt = vt.cursor()
     liste = svt.execute(f"SELECT * FROM isimler")
-
+    
     # icerik = QVBoxLayout()
     icerik = QGridLayout()
     x = 1
     icerik.addWidget(QLabel('Adı'),0,1)
-    icerik.addWidget(QLabel('Soyadı'),0,2)
-    icerik.addWidget(QLabel('Numarası'),0,3)
-    
-    for a in liste:
-      print (a[1],a[2],a[3])
-      icerik.addWidget(QLabel(a[1]),x,1) # gridLayout taki x.satır ve 1.sütuna QLable yerleştir.
-      icerik.addWidget(QLabel(a[2]),x,2)
-      icerik.addWidget(QLabel(a[3]),x,3)
-      x+=1
+    icerik.addWidget(QLabel('Numarası'),0,2)
+    for a in liste: 
+        print (a[1],a[2])
+        icerik.addWidget(QLabel(a[1]),x,1) # gridLayout taki x.satır ve 1.sütuna QLable yerleştir.
+        icerik.addWidget(QLabel(a[2]),x,2)
+        x+=1
 
     # icerik.setColumnStretch(0, 2) # buna benzer bir şey ile hücre birleştirmesi yapılabilir.
     self.d1 = QPushButton('Ana ekrana dön')
     icerik.addWidget(self.d1,x,1) # x.satır ve 1.sütuna self.d1 widgetini yerleştir.
-    self.d1.clicked.connect(self.anaEkranaDon)    
+    self.d1.clicked.connect(self.anaEkranaDon)
+
     araclar = QWidget() # Pencere widgeti oluştur.
     araclar.setLayout(icerik) # Pencere widgeti için layout ata
     self.setCentralWidget(araclar) # pencere widgeti ana layatunu ata
     vt.close()
 
   def anaEkranaDon():
-      self.close() # mevcut pencereyi kapa
-      self.ae = anaEkran('Ana ekran') # anaekran isimli pencere tanımla
-      self.ae.show() # anaekran ı göster.
+    self.close() # mevcut pencereyi kapa
+    self.ae = anaEkran('Ana ekran') # anaekran isimli pencere tanımla
+    self.ae.show() # anaekran ı göster.
 
 sifreOlustur()
 uygulama = QApplication([])
